3ce8280c858da1e8a0f0b6fd17238160a8b0d9ee
Michael Akinde <michael.akinde@statnett.no>
2024-06-13 12:23:43 +0200

    Fix breaking TC settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,16 +1,21 @@
 import no.elhub.devxp.build.configuration.pipeline.ElhubProject.Companion.elhubProject
 import no.elhub.devxp.build.configuration.pipeline.constants.Group.DEVXP
+import no.elhub.devxp.build.configuration.pipeline.constants.ArtifactoryRepository
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleAutoRelease
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleVerify
 
 elhubProject(DEVXP, "devxp-auto-changelog") {
+    val artifactoryRepository = ArtifactoryRepository.ELHUB_BIN_RELEASE_LOCAL
 
     pipeline {
         sequential {
             val artifacts = gradleVerify {
                 analyzeDependencies = false
+                modules = listOf("cli", "core")
             }
-            gradleAutoRelease(artifacts = listOf(artifacts))
+            gradleAutoRelease(artifacts = listOf(artifacts)) {
+                repository = artifactoryRepository
+            }
         }
     }
 }
