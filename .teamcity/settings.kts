78e70970eccb4aa4bfe3df1e8c6514a7df474306
Johannes Schaanning <johannes.schaanning@statnett.no>
2025-01-24 14:46:28 +0100

    Update teamcity files to reflect new structure

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,3 +1,4 @@
+import jetbrains.buildServer.configs.kotlin.ArtifactRule
 import no.elhub.devxp.build.configuration.pipeline.ElhubProject.Companion.elhubProject
 import no.elhub.devxp.build.configuration.pipeline.constants.Group.DEVXP
 import no.elhub.devxp.build.configuration.pipeline.constants.ArtifactoryRepository
@@ -5,13 +6,16 @@ import no.elhub.devxp.build.configuration.pipeline.jobs.gradleAutoRelease
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleVerify
 
 elhubProject(DEVXP, "devxp-auto-changelog") {
+    val gradleModules = listOf("core", "cli")
     val artifactoryRepository = ArtifactoryRepository.ELHUB_BIN_RELEASE_LOCAL
 
     pipeline {
         sequential {
             val artifacts = gradleVerify {
                 analyzeDependencies = false
-                modules = listOf("cli", "core")
+                modules = gradleModules
+                buildArtifactRules = gradleModules.map { ArtifactRule.include("$it/build", "$it/build.zip") }
+                outputArtifactRules = gradleModules.map { ArtifactRule.include("$it/build.zip!**", "$it/build") }
             }
             gradleAutoRelease(artifacts = listOf(artifacts)) {
                 repository = artifactoryRepository
