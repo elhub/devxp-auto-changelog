b6e8fd3763514125e6dd66563b74021396bf1942
Johannes Schaanning <johannes.schaanning@statnett.no>
2025-01-24 13:02:04 +0100

    Update gradle versions and clean up structure [patch]

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,28 +1,38 @@
-plugins {
-}
+group = ""
+
+val applicationMainClass: String by project
 
 dependencies {
     implementation(project(":core"))
     implementation(libs.cli.picocli)
 }
 
-group = ""
-
-val applicationMainClass : String by project
-
-val fatJar by tasks.creating(type = Jar::class) {
-    manifest {
-        attributes["Implementation-Title"] = rootProject.name
-        attributes["Implementation-Version"] = rootProject.version
-        attributes["Main-Class"] = applicationMainClass
+artifactory {
+    publish {
+        repository {
+            repoKey = "elhub-bin-release-local"
+        }
     }
-    from(configurations.compileClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA", "*.html")
-    with(tasks.jar.get() as CopySpec)
-    mustRunAfter(tasks["jar"])
-    duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
 
-tasks["assemble"].dependsOn("fatJar")
+val shadowJar by tasks.getting(com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar::class) {
+    archiveBaseName.set(rootProject.name)
+    archiveClassifier.set("")
+    isZip64 = true
+    manifest {
+        attributes(
+            mapOf(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version,
+                "Main-Class" to applicationMainClass
+            )
+        )
+    }
+    from(project.configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
+    mergeServiceFiles("META-INF/cxf/bus-extensions.txt")
+    with(tasks.jar.get() as CopySpec)
+    dependsOn(tasks.jar)
+}
 
-tasks["generateMetadataFileForCliPublication"].dependsOn("fatJar")
+tasks["assemble"].dependsOn("shadowJar")
