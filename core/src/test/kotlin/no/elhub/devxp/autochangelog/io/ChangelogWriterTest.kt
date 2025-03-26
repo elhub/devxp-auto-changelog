f62548106f702d0907969246b943d6175fdffcc7
Johannes Schaanning <johannes.schaanning@statnett.no>
2025-03-26 08:46:44 +0100

    remove unnecessary test context

--- core/src/test/kotlin/no/elhub/devxp/autochangelog/io/ChangelogWriterTest.kt
+++ core/src/test/kotlin/no/elhub/devxp/autochangelog/io/ChangelogWriterTest.kt
@@ -6,16 +6,14 @@ import io.kotest.matchers.shouldBe
 import no.elhub.devxp.autochangelog.project.defaultContent
 
 class ChangelogWriterTest : FunSpec({
-    context("ChangelogWriter") {
-
-        context("write with non-empty original changelog contents") {
-            val defaultDescription = """
+    context("write with non-empty original changelog contents") {
+        val defaultDescription = """
                 # Changelog
 
                 All notable changes to this project will be documented in this file.
             """.trimIndent()
 
-            val existingContent = """
+        val existingContent = """
                 ## [1.1.0] - 2024-02-15
 
                 ### Added
@@ -27,43 +25,42 @@ class ChangelogWriterTest : FunSpec({
                 - All the code
             """.trimIndent()
 
-            val writer = ChangelogWriter(start = defaultDescription, end = existingContent)
+        val writer = ChangelogWriter(start = defaultDescription, end = existingContent)
 
-            test("should append new content after default description") {
-                val s = writer.writeToString(singleChangelist)
-                s shouldBe """
+        test("should append new content after default description") {
+            val s = writer.writeToString(singleChangelist)
+            s shouldBe """
                     |$defaultDescription
                     |
                     |$singleExpectedMd
                     |
                     |$existingContent
                 """.trimMargin()
-            }
         }
+    }
 
-        context("write with empty original changelog contents") {
-            val writer = ChangelogWriter()
+    context("write with empty original changelog contents") {
+        val writer = ChangelogWriter()
 
-            listOf(singleChangelogTestMd, doubleChangeLogTestMd).map {
-                test(it.name) {
-                    val s = writer.writeToString(it.changelist)
-                    s shouldBe """
+        listOf(singleChangelogTestMd, doubleChangeLogTestMd).map {
+            test(it.name) {
+                val s = writer.writeToString(it.changelist)
+                s shouldBe """
                         |${defaultContent.joinToString("\n")}
                         |
                         |${it.expected}
                     """.trimMargin()
-                }
             }
+        }
 
-            test("writeToJson() should produce valid json") {
-                val s = writer.writeToJson(singleChangelist)
-                s.shouldBeValidJson()
-            }
+        test("writeToJson() should produce valid json") {
+            val s = writer.writeToJson(singleChangelist)
+            s.shouldBeValidJson()
+        }
 
-            listOf(singleChangelogTestJson, doubleChangeLogTestJson).map {
-                test(it.name) {
-                    writer.writeToJson(it.changelist) shouldBe it.expected
-                }
+        listOf(singleChangelogTestJson, doubleChangeLogTestJson).map {
+            test(it.name) {
+                writer.writeToJson(it.changelist) shouldBe it.expected
             }
         }
     }
