ea567f0193e9ab83f1274ce815ec3c66fcfdab96
Michael Akinde <michael.akinde@statnett.no>
2025-09-23 15:49:16 +0200

    Ensure both modules are scanned in Sonarqube

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -14,6 +14,10 @@ elhubProject(DEVXP, "devxp-auto-changelog") {
                 modules = gradleModules
                 buildArtifactRules = gradleModules.map { ArtifactRule.include("$it/build", "$it/build.zip") }
                 outputArtifactRules = gradleModules.map { ArtifactRule.include("$it/build.zip!**", "$it/build") }
+                sonarScanSettings = {
+                    sonarProjectSources = "build.gradle.kts,settings.gradle.kts,cli/src/main,cli/src/build.gradle.kts,core/src/main,core/src/build.gradle.kts"
+                    sonarProjectTests = "cli/src/test,core/src/test"
+                }
                 enablePublishMetrics = true
             }
             gradleAutoRelease(artifacts = listOf(artifacts)) {
