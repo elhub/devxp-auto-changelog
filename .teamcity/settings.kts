5edf87828dd22c1645e28cbe6a044715e25a27e2
Michael Akinde <michael.akinde@statnett.no>
2025-09-23 16:42:10 +0200

    Fix directory

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -15,7 +15,7 @@ elhubProject(DEVXP, "devxp-auto-changelog") {
                 buildArtifactRules = gradleModules.map { ArtifactRule.include("$it/build", "$it/build.zip") }
                 outputArtifactRules = gradleModules.map { ArtifactRule.include("$it/build.zip!**", "$it/build") }
                 sonarScanSettings = {
-                    sonarProjectSources = "build.gradle.kts,settings.gradle.kts,cli/src/main,cli/src/build.gradle.kts,core/src/main,core/src/build.gradle.kts"
+                    sonarProjectSources = "build.gradle.kts,settings.gradle.kts,cli/src/main,cli/build.gradle.kts,core/src/main,core/build.gradle.kts"
                     sonarProjectTests = "cli/src/test,core/src/test"
                 }
                 enablePublishMetrics = true
