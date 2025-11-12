dcf98d05abbe233eea00d761f34624dae414db1c
Michael Akinde <michael.akinde@statnett.no>
2025-11-12 18:47:23 +0100

    Fix the gradle setup

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,72 +1,10 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    alias(libs.plugins.application)
-    alias(libs.plugins.shadow)
+    alias(libs.plugins.elhub.gradle.plugin.application)
+    alias(libs.plugins.kotlin.plugin.serialization)
 }
 
 dependencies {
     implementation(project(":core"))
     implementation(libs.cli.picocli)
-}
-
-val applicationMainClass: String by project
-
-application {
-    mainClass.set(applicationMainClass)
-}
-
-tasks.jar { enabled = false }
-
-val shadowJar by tasks.getting(com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar::class) {
-    archiveBaseName.set(rootProject.name)
-    archiveClassifier.set("")
-    isZip64 = true
-    manifest {
-        attributes(
-            mapOf(
-                "Implementation-Title" to project.name,
-                "Implementation-Version" to project.version,
-                "Main-Class" to "no/elhub/devxp/autochangelog/cli/AutoChangelog.kt",
-            ),
-        )
-    }
-    from(
-        project.configurations.runtimeClasspath
-            .get()
-            .map { if (it.isDirectory) it else zipTree(it) },
-    )
-    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
-    mergeServiceFiles("META-INF/cxf/bus-extensions.txt")
-    with(tasks.jar.get() as CopySpec)
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            all {
-                this as MavenPublication
-                if (name == rootProject.name) {
-                    artifacts.clear()
-                    artifact(shadowJar)
-                }
-            }
-        }
-    }
-}
-
-tasks.artifactoryPublish {
-    dependsOn(shadowJar)
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget = JvmTarget.JVM_17
-        javaParameters = true
-    }
+    testImplementation(libs.test.kotest.runner.junit5)
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,29 +1,22 @@
-group = "no.elhub.devxp"
-
 plugins {
-    alias(libs.plugins.serialization)
-}
-
-kotlin {
-    sourceSets {
-        all {
-            languageSettings.optIn("kotlinx.serialization.ExperimentalSerializationApi")
-        }
-    }
+    alias(libs.plugins.elhub.gradle.plugin.library)
+    alias(libs.plugins.kotlin.plugin.serialization)
+    // alias(libs.plugins.kotlin.plugin.serialization)
 }
 
 dependencies {
-    val implementation by configurations
-    val testImplementation by configurations
-
+    api(rootProject.libs.git.jgit)
+    api(rootProject.libs.git.jgit.ssh)
+    api(libs.kotlinx.serialization.json)
     implementation(libs.bundles.logging)
-    implementation(libs.kotlinx.serialization.json)
     implementation(libs.apache.commons.io)
-    implementation(libs.test.kotlin.faker)
-    implementation(libs.test.mockk)
+    testImplementation(libs.test.kotest.runner.junit5)
+    testImplementation(libs.test.kotlin.faker)
+    testImplementation(libs.test.mockk)
     testImplementation(libs.test.kotest.assertions.json)
 }
 
+// Do not publish library artifacts to Artifactory
 tasks.artifactoryPublish {
     skip = true
 }
